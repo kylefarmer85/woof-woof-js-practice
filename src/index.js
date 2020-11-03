d108c5a4ad668134e3aa2c6069150b6256f4604b
kylefarmer85 <kylefarmer85@gmail.com>
2020-11-03 12:12:44 -0600

    all deliverables met

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 function main() {
+  fetchDogs()
   divDogBarEventListener()
 }
 
 const DogsURL = "http://localhost:3000/pups"
 const divDogBar = document.querySelector('#dog-bar')
+const filterBtn = document.querySelector('#good-dog-filter')
 
-fetch(DogsURL)
-.then(resp => resp.json())
-.then(dogs => renderDogs(dogs))
+function fetchDogs() {
+  divDogBar.innerHTML ='';
+  fetch(DogsURL)
+  .then(resp => resp.json())
+  .then(dogs => renderDogs(dogs))
+}
 
 function renderDogs(dogs) {
   dogs.forEach(dog => {
+  if (filterBtn.innerText.includes("OFF")) {
     const dogSpan = document.createElement('span')
     dogSpan.innerText = dog.name
     dogSpan.className = "dog-span"
     dogSpan.dataset.id = dog.id
     divDogBar.append(dogSpan)
+  } else {
+    if (dog.isGoodDog) {
+      const dogSpan = document.createElement('span')
+      dogSpan.innerText = dog.name
+      dogSpan.className = "dog-span"
+      dogSpan.dataset.id = dog.id
+      divDogBar.append(dogSpan)
+    }
+  }
   })
+}
+
+filterBtn.addEventListener("click", () => {
+  if (filterBtn.innerText === "Filter good dogs: OFF") {
+    filterBtn.innerText = "Filter good dogs: ON";
+    fetchDogs();
+  } else {
+    filterBtn.innerText = "Filter good dogs: OFF";
+    fetchDogs();
+  }
+})
+
+function filterDogBar() {
+
 }
 
 function divDogBarEventListener() {
